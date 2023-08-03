12f18cecd20b07c6491157e2dd22001866e7c453
Z3BRO <101530102+Z3BRO@users.noreply.github.com>
2023-08-03 23:37:42 +0200

    Updating pretty much everything
    Huge thanks to xMasterX for rewriting the rgb patch.

--- RGB FW FILES/applications/services/notification/notification_app.c
+++ RGB FW FILES/applications/services/notification/notification_app.c
@@ -3,6 +3,9 @@
 #include <furi_hal.h>
 #include <storage/storage.h>
 #include <input/input.h>
+#include <gui/gui_i.h>
+#include <u8g2_glue.h>
+#include <lib/toolbox/float_tools.h>
 #include "notification.h"
 #include "notification_messages.h"
 #include "notification_app.h"
@@ -21,14 +24,14 @@ static const uint8_t reset_sound_mask = 1 << 4;
 static const uint8_t reset_display_mask = 1 << 5;
 static const uint8_t reset_blink_mask = 1 << 6;
 
-void notification_vibro_on(bool force);
-void notification_vibro_off();
-void notification_sound_on(float freq, float volume, bool force);
-void notification_sound_off();
+static void notification_vibro_on(bool force);
+static void notification_vibro_off();
+static void notification_sound_on(float freq, float volume, bool force);
+static void notification_sound_off();
 
-uint8_t notification_settings_get_display_brightness(NotificationApp* app, uint8_t value);
-uint8_t notification_settings_get_rgb_led_brightness(NotificationApp* app, uint8_t value);
-uint32_t notification_settings_display_off_delay_ticks(NotificationApp* app);
+static uint8_t notification_settings_get_display_brightness(NotificationApp* app, uint8_t value);
+static uint8_t notification_settings_get_rgb_led_brightness(NotificationApp* app, uint8_t value);
+static uint32_t notification_settings_display_off_delay_ticks(NotificationApp* app);
 
 void notification_message_save_settings(NotificationApp* app) {
     NotificationAppMessage m = {
@@ -40,7 +43,8 @@ void notification_message_save_settings(NotificationApp* app) {
 };
 
 // internal layer
-void notification_apply_internal_led_layer(NotificationLedLayer* layer, uint8_t layer_value) {
+static void
+    notification_apply_internal_led_layer(NotificationLedLayer* layer, uint8_t layer_value) {
     furi_assert(layer);
     furi_assert(layer->index < LayerMAX);
 
@@ -53,7 +57,13 @@ void notification_apply_internal_led_layer(NotificationLedLayer* layer, uint8_t 
     }
 }
 
-bool notification_is_any_led_layer_internal_and_not_empty(NotificationApp* app) {
+static void notification_apply_lcd_contrast(NotificationApp* app) {
+    Gui* gui = furi_record_open(RECORD_GUI);
+    u8x8_d_st756x_set_contrast(&gui->canvas->fb.u8x8, app->settings.contrast);
+    furi_record_close(RECORD_GUI);
+}
+
+static bool notification_is_any_led_layer_internal_and_not_empty(NotificationApp* app) {
     bool result = false;
     if((app->led[0].index == LayerInternal) || (app->led[1].index == LayerInternal) ||
        (app->led[2].index == LayerInternal)) {
@@ -68,7 +78,7 @@ bool notification_is_any_led_layer_internal_and_not_empty(NotificationApp* app) 
 }
 
 // notification layer
-void notification_apply_notification_led_layer(
+static void notification_apply_notification_led_layer(
     NotificationLedLayer* layer,
     const uint8_t layer_value) {
     furi_assert(layer);
@@ -82,7 +92,7 @@ void notification_apply_notification_led_layer(
     furi_hal_light_set(layer->light, layer->value[LayerNotification]);
 }
 
-void notification_reset_notification_led_layer(NotificationLedLayer* layer) {
+static void notification_reset_notification_led_layer(NotificationLedLayer* layer) {
     furi_assert(layer);
     furi_assert(layer->index < LayerMAX);
 
@@ -95,7 +105,10 @@ void notification_reset_notification_led_layer(NotificationLedLayer* layer) {
     furi_hal_light_set(layer->light, layer->value[LayerInternal]);
 }
 
-void notification_reset_notification_layer(NotificationApp* app, uint8_t reset_mask) {
+static void notification_reset_notification_layer(
+    NotificationApp* app,
+    uint8_t reset_mask,
+    float display_brightness_set) {
     if(reset_mask & reset_blink_mask) {
         furi_hal_light_blink_stop();
     }
@@ -115,6 +128,9 @@ void notification_reset_notification_layer(NotificationApp* app, uint8_t reset_m
         notification_sound_off();
     }
     if(reset_mask & reset_display_mask) {
+        if(!float_is_equal(display_brightness_set, app->settings.display_brightness)) {
+            furi_hal_light_set(LightBacklight, app->settings.display_brightness * 0xFF);
+        }
         furi_timer_start(app->display_timer, notification_settings_display_off_delay_ticks(app));
     }
 }
@@ -131,28 +147,28 @@ uint8_t notification_settings_get_display_brightness(NotificationApp* app, uint8
     return (value * app->settings.display_brightness);
 }
 
-uint8_t notification_settings_get_rgb_led_brightness(NotificationApp* app, uint8_t value) {
+static uint8_t notification_settings_get_rgb_led_brightness(NotificationApp* app, uint8_t value) {
     return (value * app->settings.led_brightness);
 }
 
-uint32_t notification_settings_display_off_delay_ticks(NotificationApp* app) {
+static uint32_t notification_settings_display_off_delay_ticks(NotificationApp* app) {
     return (
         (float)(app->settings.display_off_delay_ms) /
         (1000.0f / furi_kernel_get_tick_frequency()));
 }
 
 // generics
-void notification_vibro_on(bool force) {
+static void notification_vibro_on(bool force) {
     if(!furi_hal_rtc_is_flag_set(FuriHalRtcFlagStealthMode) || force) {
         furi_hal_vibro_on(true);
     }
 }
 
-void notification_vibro_off() {
+static void notification_vibro_off() {
     furi_hal_vibro_on(false);
 }
 
-void notification_sound_on(float freq, float volume, bool force) {
+static void notification_sound_on(float freq, float volume, bool force) {
     if(!furi_hal_rtc_is_flag_set(FuriHalRtcFlagStealthMode) || force) {
         if(furi_hal_speaker_is_mine() || furi_hal_speaker_acquire(30)) {
             furi_hal_speaker_start(freq, volume);
@@ -160,7 +176,7 @@ void notification_sound_on(float freq, float volume, bool force) {
     }
 }
 
-void notification_sound_off() {
+static void notification_sound_off() {
     if(furi_hal_speaker_is_mine()) {
         furi_hal_speaker_stop();
         furi_hal_speaker_release();
@@ -175,7 +191,7 @@ static void notification_display_timer(void* ctx) {
 }
 
 // message processing
-void notification_process_notification_message(
+static void notification_process_notification_message(
     NotificationApp* app,
     NotificationAppMessage* message) {
     uint32_t notification_message_index = 0;
@@ -203,13 +219,14 @@ void notification_process_notification_message(
                 notification_apply_notification_led_layer(
                     &app->display,
                     notification_message->data.led.value * display_brightness_setting);
+                reset_mask |= reset_display_mask;
             } else {
+                reset_mask &= ~reset_display_mask;
                 notification_reset_notification_led_layer(&app->display);
                 if(furi_timer_is_running(app->display_timer)) {
                     furi_timer_stop(app->display_timer);
                 }
             }
-            reset_mask |= reset_display_mask;
             break;
         case NotificationMessageTypeLedDisplayBacklightEnforceOn:
             furi_assert(app->display_led_lock < UINT8_MAX);
@@ -334,6 +351,9 @@ void notification_process_notification_message(
             reset_mask |= reset_green_mask;
             reset_mask |= reset_blue_mask;
             break;
+        case NotificationMessageTypeLcdContrastUpdate:
+            notification_apply_lcd_contrast(app);
+            break;
         }
         notification_message_index++;
         notification_message = (*message->sequence)[notification_message_index];
@@ -358,11 +378,12 @@ void notification_process_notification_message(
     }
 
     if(reset_notifications) {
-        notification_reset_notification_layer(app, reset_mask);
+        notification_reset_notification_layer(app, reset_mask, display_brightness_setting);
     }
 }
 
-void notification_process_internal_message(NotificationApp* app, NotificationAppMessage* message) {
+static void
+    notification_process_internal_message(NotificationApp* app, NotificationAppMessage* message) {
     uint32_t notification_message_index = 0;
     const NotificationMessage* notification_message;
     notification_message = (*message->sequence)[notification_message_index];
@@ -549,6 +570,7 @@ int32_t notification_srv(void* p) {
     notification_apply_internal_led_layer(&app->led[0], 0x00);
     notification_apply_internal_led_layer(&app->led[1], 0x00);
     notification_apply_internal_led_layer(&app->led[2], 0x00);
+    notification_apply_lcd_contrast(app);
 
     furi_record_create(RECORD_NOTIFICATION, app);
 
--- RGB FW FILES/applications/settings/notification_settings/notification_settings_app.c
+++ RGB FW FILES/applications/settings/notification_settings/notification_settings_app.c
@@ -21,31 +21,53 @@ static const NotificationSequence sequence_note_c = {
     NULL,
 };
 
-#define BACKLIGHT_COUNT 5
-const char* const backlight_text[BACKLIGHT_COUNT] = {
-    "0%",
-    "25%",
-    "50%",
-    "75%",
-    "100%",
+#define CONTRAST_COUNT 11
+const char* const contrast_text[CONTRAST_COUNT] = {
+    "-5",
+    "-4",
+    "-3",
+    "-2",
+    "-1",
+    "0",
+    "+1",
+    "+2",
+    "+3",
+    "+4",
+    "+5",
 };
-const float backlight_value[BACKLIGHT_COUNT] = {
-    0.0f,
-    0.25f,
-    0.5f,
-    0.75f,
-    1.0f,
+const int32_t contrast_value[CONTRAST_COUNT] = {
+    -5,
+    -4,
+    -3,
+    -2,
+    -1,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
 };
 
-#define VOLUME_COUNT 5
-const char* const volume_text[VOLUME_COUNT] = {
-    "0%",
-    "25%",
-    "50%",
-    "75%",
-    "100%",
+#define BACKLIGHT_COUNT 21
+const char* const backlight_text[BACKLIGHT_COUNT] = {
+    "0%",  "5%",  "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%",  "50%",
+    "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%", "100%",
 };
-const float volume_value[VOLUME_COUNT] = {0.0f, 0.25f, 0.5f, 0.75f, 1.0f};
+const float backlight_value[BACKLIGHT_COUNT] = {
+    0.00f, 0.05f, 0.10f, 0.15f, 0.20f, 0.25f, 0.30f, 0.35f, 0.40f, 0.45f, 0.50f,
+    0.55f, 0.60f, 0.65f, 0.70f, 0.75f, 0.80f, 0.85f, 0.90f, 0.95f, 1.00f,
+};
+
+#define VOLUME_COUNT 21
+const char* const volume_text[VOLUME_COUNT] = {
+    "0%",  "5%",  "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%",  "50%",
+    "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%", "100%",
+};
+const float volume_value[VOLUME_COUNT] = {
+    0.00f, 0.05f, 0.10f, 0.15f, 0.20f, 0.25f, 0.30f, 0.35f, 0.40f, 0.45f, 0.50f,
+    0.55f, 0.60f, 0.65f, 0.70f, 0.75f, 0.80f, 0.85f, 0.90f, 0.95f, 1.00f,
+};
 
 #define DELAY_COUNT 11
 const char* const delay_text[DELAY_COUNT] = {
@@ -71,9 +93,19 @@ const char* const vibro_text[VIBRO_COUNT] = {
 };
 const bool vibro_value[VIBRO_COUNT] = {false, true};
 
+static void contrast_changed(VariableItem* item) {
+    NotificationAppSettings* app = variable_item_get_context(item);
+    uint8_t index = variable_item_get_current_value_index(item);
+
+    variable_item_set_current_value_text(item, contrast_text[index]);
+    app->notification->settings.contrast = contrast_value[index];
+    notification_message(app->notification, &sequence_lcd_contrast_update);
+}
+
 static void backlight_changed(VariableItem* item) {
     NotificationAppSettings* app = variable_item_get_context(item);
     uint8_t index = variable_item_get_current_value_index(item);
+
     variable_item_set_current_value_text(item, backlight_text[index]);
     app->notification->settings.display_brightness = backlight_value[index];
     notification_message(app->notification, &sequence_display_backlight_on);
@@ -149,6 +181,13 @@ static NotificationAppSettings* alloc_settings() {
 
     VariableItem* item;
     uint8_t value_index;
+
+    item = variable_item_list_add(
+        app->variable_item_list, "LCD Contrast", CONTRAST_COUNT, contrast_changed, app);
+    value_index =
+        value_index_int32(app->notification->settings.contrast, contrast_value, CONTRAST_COUNT);
+    variable_item_set_current_value_index(item, value_index);
+    variable_item_set_current_value_text(item, contrast_text[value_index]);
 
     item = variable_item_list_add(
         app->variable_item_list, "LCD Color", rgb_backlight_get_color_count(), color_changed, app);
@@ -229,7 +268,6 @@ int32_t notification_settings_app(void* p) {
     NotificationAppSettings* app = alloc_settings();
     view_dispatcher_run(app->view_dispatcher);
     notification_message_save_settings(app->notification);
-
     free_settings(app);
     return 0;
 }
